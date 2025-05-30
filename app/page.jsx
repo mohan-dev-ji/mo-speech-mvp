65cd70c4a27de7db704be7824cf8f9e78c9b296f
Mohan <moveraitch@gmail.com>
2025-05-30 22:31:40 +0100

    under construction

--- app/page.jsx
+++ app/page.jsx
@@ -16,24 +16,33 @@ import Navbar from "./components/sections/Navbar";
 
 export default function LandingPage() {
   return (
-    <div className="flex min-h-screen flex-col max-w-[1440px] mx-auto w-full">
-      <Navbar />
 
-      <main className="flex-1">
-        {/* Hero Section */}
-        <HeroSection />
-        <ChromeSection />
-        <AboutSection />
-        <FeaturesSection />
-        <WhosItForSection />
-        <HowItWorks />
-        <WithoutExpectation />
-        <WhenAndWhereSection />
-        <FutureVisionSection />
-        <ContactSection />
-        <FinalCta />
-      </main>
-      <Footer />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 text-center">
+      <h1 className="text-1xl md:text-5xl font-bold text-gray-800 mb-4">
+        Mo Speech is getting an important update
+      </h1>
+      <p className="text-lg md:text-xl text-gray-600 max-w-xl">
+        We're making changes to bring you a better, more compliant experience. Thanks for your patience while we get things ready.
+      </p>
     </div>
+    // <div className="flex min-h-screen flex-col max-w-[1440px] mx-auto w-full">
+    //   <Navbar />
+
+    //   <main className="flex-1">
+    //     {/* Hero Section */}
+    //     <HeroSection />
+    //     <ChromeSection />
+    //     <AboutSection />
+    //     <FeaturesSection />
+    //     <WhosItForSection />
+    //     <HowItWorks />
+    //     <WithoutExpectation />
+    //     <WhenAndWhereSection />
+    //     <FutureVisionSection />
+    //     <ContactSection />
+    //     <FinalCta />
+    //   </main>
+    //   <Footer />
+    // </div>
   );
 } 
